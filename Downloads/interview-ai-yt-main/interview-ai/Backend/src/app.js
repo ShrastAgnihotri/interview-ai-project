04ca61a25ff5696d3087bef8970d5adf818a63ba
ShrastiAgnihotri <maniagnihotri212@gmail.com>
2026-04-27 17:05:59 +0530

    Bypassing CORS variable for final fix

--- Downloads/interview-ai-yt-main/interview-ai/Backend/src/app.js
+++ Downloads/interview-ai-yt-main/interview-ai/Backend/src/app.js
@@ -7,7 +7,8 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    // origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [process.env.FRONTEND_URL, "https://interview-ai-project-delta.vercel.app", "http://localhost:5173"],
     credentials: true
 }))
 
